58501979cdfbe0d2a147d4c03f5c4335ae2007c7
Cassiano Simas Miranda <csimasmi@emeal.nttdata.com>
2022-05-02 16:29:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appfilmes/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.appfilmes.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appfilmes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
